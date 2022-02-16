cb366094aaf6a1d6031a7986f77ff19048d7543d
krzysztofrz <krzysztof.rzeszut@gmail.com>
2022-02-16 10:26:16 +0100

    header complete

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 
+// const header = document.querySelector(".header");
+//
+// if (window.scrollY > 100) {
+//     header.classList.add("header-shrink");
+// } else {
+//     header.classList.remove("header-shrink");
+// }
+//
+// window.addEventListener("scroll", function(e) {
+//     if (window.scrollY > 100) {
+//         header.classList.add("header-shrink");
+//     } else {
+//         header.classList.remove("header-shrink");
+//     }
+// })
